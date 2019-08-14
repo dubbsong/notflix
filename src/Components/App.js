bb4831317634b985ecf1f82ef3498eccc7f6c90d
dubbsong <dubbsong@gmail.com>
2019-08-14 11:53:43 +0900

    1st commit

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <React.Fragment>
+        <h2>오많배</h2>
+      </React.Fragment>
+    );
+  }
+}
+
+export default App;
