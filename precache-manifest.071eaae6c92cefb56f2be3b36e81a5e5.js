5ab670320c7614b637da6ad3b5febe647f391bfb
dubbsong <dubbsong@gmail.com>
2019-08-14 17:59:59 +0900

    Updates

---
+++ precache-manifest.071eaae6c92cefb56f2be3b36e81a5e5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8400fe85a480216e2b3c576f7d1554da",
+    "url": "/notflix/index.html"
+  },
+  {
+    "revision": "fe6593943ca3bdef7a32",
+    "url": "/notflix/static/js/2.8aac0c65.chunk.js"
+  },
+  {
+    "revision": "dc22dd451f5ecac9f877",
+    "url": "/notflix/static/js/main.4d9f0959.chunk.js"
+  },
+  {
+    "revision": "03c89c6a5dfdb3b2f95e",
+    "url": "/notflix/static/js/runtime~main.35d25a23.js"
+  },
+  {
+    "revision": "f15e2edaaa1ebaea9fc9830f745e0e9d",
+    "url": "/notflix/static/media/bg_movie.f15e2eda.webp"
+  },
+  {
+    "revision": "bd8ecc9b1792a12eccd809ae154562bf",
+    "url": "/notflix/static/media/bg_tv.bd8ecc9b.webp"
+  },
+  {
+    "revision": "854b40f2581c25b588c3e81a406c5595",
+    "url": "/notflix/static/media/logo.854b40f2.png"
+  },
+  {
+    "revision": "41b45fdce09bd6acd07c7a8949da675e",
+    "url": "/notflix/static/media/logo_sm.41b45fdc.png"
+  },
+  {
+    "revision": "a741d5a3e1341a6cb069d1e85abfbb7a",
+    "url": "/notflix/static/media/omb.a741d5a3.png"
+  }
+]);
